a0109e7bb87e72b201d7c16c910f657db13831cb
otakustay <otakustay@gmail.com>
2015-12-19 19:24:27 +0800

    修复webserver-config

--- edp-webserver-config.js
+++ edp-webserver-config.js
@@ -23,7 +23,7 @@ exports.getLocations = function () {
         {
             // All source and spec files
             key: 'source',
-            location: /\.js(\?.+)?/,
+            location: /^\/(src|test)(\/[^\/]+)*\.js\?/,
             handler: [
                 babel(BABEL_OPTIONS)
             ]
