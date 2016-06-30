25b4371dd6d2657fa157fd7bb8e497e89403528f
otakustay <otakustay@gmail.com>
2016-06-30 16:09:00 +0800

    更新gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
-let gulp = require('gulp');
-let sourcemaps = require('gulp-sourcemaps');
-let babel = require('gulp-babel');
-let replace = require('gulp-replace');
-let uglify = require('gulp-uglify');
-let rename = require("gulp-rename");
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var replace = require('gulp-replace');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var merge = require('merge-stream');
 
 gulp.task(
     'default',
-    () => {
-        gulp.src('src/Model.js')
+    function () {
+        var development = gulp.src('src/Model.js')
             .pipe(replace('process.env.NODE_ENV', '"development"'))
             .pipe(sourcemaps.init())
             .pipe(babel())
             .pipe(sourcemaps.write('map'))
             .pipe(gulp.dest('.'));
 
-        gulp.src('src/Model.js')
+        var production = gulp.src('src/Model.js')
             .pipe(replace('process.env.NODE_ENV', '"production"'))
             .pipe(sourcemaps.init())
             .pipe(babel())
@@ -23,5 +24,7 @@ gulp.task(
             .pipe(rename({suffix: '.min'}))
             .pipe(sourcemaps.write('map'))
             .pipe(gulp.dest('.'));
+
+        return merge(development, production);
     }
 );
